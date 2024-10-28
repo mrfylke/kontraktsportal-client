984b866542444e54377e2bcc440f2b0706fe6d29
mortennordseth <morten@nordseth.net>
2024-10-28 10:16:57 +0100

    refactor: extract form action to function

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,16 +2,15 @@ import { signIn } from '@/modules/authentication';
 import styles from './page.module.css';
 
 export default function SignInPage() {
+  const handleSignIn = async () => {
+    'use server';
+    await signIn('microsoft-entra-id', { redirectTo: '/' });
+  };
   return (
     <main className={styles.main}>
       <h2>Logg på driftsportal</h2>
       <p>Bruk din konto i FRAM for å logge inn på driftsportalen. </p>
-      <form
-        action={async () => {
-          'use server';
-          await signIn('microsoft-entra-id', { redirectTo: '/' });
-        }}
-      >
+      <form action={handleSignIn}>
         <button type="submit">Logg på med FRAMkonto</button>
       </form>
     </main>
