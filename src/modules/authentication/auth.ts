35fd88aebdd522df4a1c3a99a90c5df73eca26d6
Adrian Santana Berg <adrian_berg96@hotmail.com>
2024-10-30 08:51:44 +0100

    feat: add custom scope to auth (#7)

--- src/modules/authentication/auth.ts
+++ src/modules/authentication/auth.ts
@@ -7,6 +7,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
       issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+      authorization: {
+        params: {
+          scope: `openid profile email ${process.env.AUTH_MICROSOFT_ENTRA_ID_SCOPE}`
+        }
+      }
     }),
   ],
 });
